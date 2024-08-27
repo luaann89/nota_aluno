3fa9dd530fcdf844f42ddb867579443cf7098012
luaann89 <127844960+luaann89@users.noreply.github.com>
2024-08-27 09:22:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+//Passo 1: Criar as constante que irão armazenar as notas do aluno
+const nota_1 = 10
+const nota_2 = 5
+const nota_3 = 4
+
+//Passo 2: Calcular a média das notas 
+const soma_notas = nota + nota_2 + nota_3
+const media = soma_notas / 3
+
+//Passo 3: Verificar se a nota é maior que 6 
+if (media >= 6) {
+    console.log('O aluno foi aprovado')
+}
+se media >= 4 && media < 6, console.log('está de prova final')
+else{
+    console.log('O aluno foi reprovado')
+}
